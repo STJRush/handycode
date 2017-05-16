41eec5302f25f33359f8ff2e044f84978e238fe5
Daniel Murray <daniel.murray@stjosephsrush.ie>
2017-05-16 10:11:20 +0100

    added more sounds

--- winsound.py
+++ winsound.py
@@ -3,3 +3,14 @@ winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
 
 # Lets you use windows sound.
 # look up winsound for other sounds
+
+"""
+Here's some more sounds
+
+'SystemAsterisk'	Asterisk
+'SystemExclamation'	Exclamation
+'SystemExit'	Exit Windows
+'SystemHand'	Critical Stop
+'SystemQuestion'	Question
+
+"""
