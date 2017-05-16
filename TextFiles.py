6761862d8529e5a684fb790fc7a08afae573068c
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-16 10:51:28 +0100

    Text Files

---
+++ TextFiles.py
@@ -0,0 +1,10 @@
+save=["insert_text_here"]
+    save.append(save)
+ 
+    target1 = open('yourlocalclimate.txt', 'w')
+ 
+    target1.write(result)
+ 
+    target1.close()
+    
+#Saving text files on Windows for Python 3.7
