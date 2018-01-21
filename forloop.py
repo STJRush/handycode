73264d5d943e696bdf2177aeb180d58b5066b294
Daniel Murray <daniel.murray@stjosephsrush.ie>
2018-01-21 14:54:46 +0000

    Loops something a number of times eg. 3 times.
    FOR THE LOOP!

---
+++ forloop.py
@@ -0,0 +1,13 @@
+for x in range(3):
+    print("ollie")
+  
+"""
+repeats stuff three times.
+output looks like:
+    
+ollie
+ollie
+ollie
+
+you can change the 3 to any number or use a variable like x.
+"""
