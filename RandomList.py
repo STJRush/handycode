384cae7bca3a88904936e83a0b1cccd1aa77072c
Daniel Murray <daniel.murray@stjosephsrush.ie>
2018-11-21 22:55:05 +0000

    Update RandomList.py

--- RandomList.py
+++ RandomList.py
@@ -1,4 +1,13 @@
 import random
 
-foo = ['a', 'b', 'c', 'd', 'e']
-print(random.choice(foo))
+foo = ['a', 'b', 'c', 'd', 'e']   # a list of whatever
+print(random.choice(foo))         # pick one at random and print it foo!
+
+
+"""
+
+What the code outputs:    a
+
+Run it again and you might get another like d or e.
+
+"""
