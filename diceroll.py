44fe7cc31fa21ea41648505b8aa592be7718c47a
Daniel Murray <daniel.murray@stjosephsrush.ie>
2020-12-09 23:54:20 +0000

    this

--- diceroll.py
+++ diceroll.py
@@ -1,16 +1,16 @@
 import random
 import statistics
 
-def d(n): #rolls a dice eg d(8) or d(20)
-  roll = random.randint(1,n)
-  return roll
+def d(n): # rolls a dice eg d(8) or d(20)
+  roll = random.randint(1,n) # makes a random integer between 0 and n
+  return roll # takes the roll back out of the fuction to use in your game)
 
-listy=[]
+listyMacListFace = []
 
-for i in range (1000):
-    listy.append(d(20))
+for x in range(9001):
+    listyMacListFace.append(d(20))
+    
+print(listyMacListFace)
 
-print(listy)
-print("mean:", statistics.mean(listy))
-
-print(d(6))
+eeenymeany = statistics.mean(listyMacListFace)
+print("mean:", eeenymeany)
---
+++ printDiceRoll.py
@@ -0,0 +1,13 @@
+import random
+
+def d(n):
+    roll = random.randint(1,6) # makes a random integer between 1 and 6
+    return roll
+
+print(d(6) + d(8))
+#d(6)
+
+print("I love the number",  d(20)")
+
+    
+
