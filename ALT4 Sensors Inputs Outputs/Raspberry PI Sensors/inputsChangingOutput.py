3d26dd2f697bff5c434283f9ecbb9d5878dfc014
Daniel Murray <daniel.murray@stjosephsrush.ie>
2021-03-02 16:35:47 +0000

    Update inputsChangingOutput.py

--- ALT4 Sensors Inputs Outputs/Raspberry PI Sensors/inputsChangingOutput.py
+++ ALT4 Sensors Inputs Outputs/Raspberry PI Sensors/inputsChangingOutput.py
@@ -8,10 +8,10 @@ GPIO.setup(24, GPIO.OUT)   # set GPIO24 as an output (LED)
 try:  
     while True:            # this will carry on until you hit CTRL+C  
         if GPIO.input(25): # if port 25 == 1  
-            print "Port 25 is 1/HIGH/True - LED ON"  
+            print("Port 25 is 1/HIGH/True - LED ON")  
             GPIO.output(24, 1)         # set port/pin value to 1/HIGH/True  
         else:  
-            print "Port 25 is 0/LOW/False - LED OFF"  
+            print("Port 25 is 0/LOW/False - LED OFF")  
             GPIO.output(24, 0)         # set port/pin value to 0/LOW/False  
         sleep(0.1)         # wait 0.1 seconds  
   
