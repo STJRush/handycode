250f30ff4051bbd52f60fa8bb10705c003b6829a
Daniel Murray <daniel.murray@stjosephsrush.ie>
2021-04-07 11:05:50 +0100

    Update thingspeaktestpy3.py

--- ALT4 Sensors Inputs Outputs/Raspberry PI Sensors/UltraSonic Water Level Sensor/thingspeaktestpy3.py
+++ ALT4 Sensors Inputs Outputs/Raspberry PI Sensors/UltraSonic Water Level Sensor/thingspeaktestpy3.py
@@ -1,11 +1,13 @@
 """ 
-thingspeaktest py3
+This Program sends a constant stream of sampleSensorReading (which is fixed to 50) up to Thingspeak to be graphed
 """ 
 import sys 
 from time import sleep 
 from urllib.request import urlopen
 
-myAPI = "JDB150GBC7UT6MG7"  #your key from your own thingspeak account. Put yours here.
+myAPI = "JDB150GBC7UT6MG2"  
+# You need to change this to your own API Key for your channel. This one is my API key and will overwrite my channel.
+# You must first make a channel on thingspeak. It'll then be under "API keys" tab as "Write API Key".
 
 
 def updateThingSpeak(): 
@@ -14,13 +16,40 @@ def updateThingSpeak():
 
    while True: 
        try: 
+            
+           # you could change this sample reading to whatever you want eg. Temperature from a sensor, score from a game, this is what you edit.
            sampleSensorReading=50 
+         
+           # This part opens up a webpage and concatinates your reading onto the end of the URL.
            f = urlopen(baseURL + "&field1=%s" % (sampleSensorReading)) 
            print (f.read()) 
            f.close() 
-           sleep(50) #uploads sensor values every 5 minutes 
+            
+           sleep(30) #uploads sensor values every 30 seconds
+         
        except: 
            print('exiting.') 
            break
 
+# runs the program here
 updateThingSpeak()
+
+
+
+"""
+Wait a second... did you just update the graph by opening a URL with 
+https://api.thingspeak.com/update?api_key=DA1K0TYF36CAE516&field1=20    ??
+
+Is that all this does?
+
+Could I technically just copy paste in the URL
+
+https://api.thingspeak.com/update?api_key=JDB150GBC7UT6MG2&field1=42
+
+into chrome to add 42 to your graph? 
+
+Why yes you magnificant reader of instructions. Yes you can.
+
+When you open that page, you'll even see the datapoint number you just sent.
+
+"""
