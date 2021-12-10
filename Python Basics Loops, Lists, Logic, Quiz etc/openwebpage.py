fc45c93fb63a9631c6263ceb06479dd31d4a8248
Daniel Murray <daniel.murray@stjosephsrush.ie>
2021-12-10 12:45:27 +0000

    Create openwebpage.py

---
+++ Python Basics Loops, Lists, Logic, Quiz etc/openwebpage.py
@@ -0,0 +1,7 @@
+import webbrowser
+choice = input("Dogs or cats???")
+
+if choice == ("dogs"):
+    webbrowser.open("https://goo.gl/images/7NyEMr")
+elif choice == ("cats"):
+    webbrowser.open("https://goo.gl/images/pnSYsp")
