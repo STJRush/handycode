6558030e1f8c3696d31dd574ae952bc9dd7c1d05
Daniel Murray <daniel.murray@stjosephsrush.ie>
2022-08-27 15:00:00 +0100

    Create loadingBarTimerASCII.py

---
+++ Python Basics Loops, Lists, Logic, Quiz etc/loadingBarTimerASCII.py
@@ -0,0 +1,7 @@
+print("Waiting a few minutes...")
+print("START_____________________________________________FINISH")
+for x in range(50):
+    print("-", end = "")
+    sleep(0.1) # change this to slow it down
+
+print("")
